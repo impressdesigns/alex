8abd87c0b36713fd7c66d3a90c9c78345e37c31e
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-06-28 14:51:03 -0500

    WIP
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

---
+++ src/main/java/com/impressdesigns/alex/Order.java
@@ -0,0 +1,17 @@
+package com.impressdesigns.alex;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/impressdesigns/alex/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.impressdesigns.alex;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface OrderRepository extends CrudRepository<Order, Integer> {
+
+}
