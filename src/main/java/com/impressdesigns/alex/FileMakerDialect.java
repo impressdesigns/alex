aa2ba1758019fb807ad92b640d4e9a0e528431fa
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-07-16 11:14:31 -0500

    FileMakerDialect accepts a Database version of 0.0.0
    This may cause errors in the future, but it clears the deprecation warning for the moment.
    
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- src/main/java/com/impressdesigns/alex/FileMakerDialect.java
+++ src/main/java/com/impressdesigns/alex/FileMakerDialect.java
@@ -1,6 +1,10 @@
 package com.impressdesigns.alex;
 
+import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
 
 public class FileMakerDialect extends Dialect {
+    public FileMakerDialect() {
+        super(DatabaseVersion.make(0));
+    }
 }
