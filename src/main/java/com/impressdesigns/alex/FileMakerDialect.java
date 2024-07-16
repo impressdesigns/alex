ad097f8427b8f1b500ae82a3460481d2a56dd7df
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-07-16 08:21:35 -0500

    Connected!
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

---
+++ src/main/java/com/impressdesigns/alex/FileMakerDialect.java
@@ -0,0 +1,10 @@
+package com.impressdesigns.alex;
+
+import org.hibernate.dialect.Dialect;
+
+public class FileMakerDialect extends Dialect {
+
+    public FileMakerDialect() {
+        super();
+    }
+}
--- src/main/java/com/impressdesigns/alex/Order.java
+++ src/main/java/com/impressdesigns/alex/Order.java
@@ -1,17 +1,19 @@
 package com.impressdesigns.alex;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "Orders")
 public class Order {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    @Column(name = "ID_Order", nullable = false, unique = true, length = 6)
+    private int order_id;
 
     public Integer getId() {
-        return id;
+        return order_id;
     }
 }
