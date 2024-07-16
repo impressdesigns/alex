82706f57db1480cc591c7c76dfa26f68721830ac
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-07-16 10:43:22 -0500

    Remove super-only constructor
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- src/main/java/com/impressdesigns/alex/FileMakerDialect.java
+++ src/main/java/com/impressdesigns/alex/FileMakerDialect.java
@@ -3,8 +3,4 @@ package com.impressdesigns.alex;
 import org.hibernate.dialect.Dialect;
 
 public class FileMakerDialect extends Dialect {
-
-    public FileMakerDialect() {
-        super();
-    }
 }
