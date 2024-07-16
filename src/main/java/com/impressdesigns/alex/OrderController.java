75bc25f09d692396f1f8c8109e75f3409a291185
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-07-16 08:46:00 -0500

    Add route to fetch a specific order
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- src/main/java/com/impressdesigns/alex/OrderController.java
+++ src/main/java/com/impressdesigns/alex/OrderController.java
@@ -3,8 +3,11 @@ package com.impressdesigns.alex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/orders")
@@ -15,5 +18,10 @@ public class OrderController {
     @GetMapping(path = "/all")
     public @ResponseBody Iterable<Order> getAllOrders() {
         return orderRepository.findAll();
+    }
+
+    @GetMapping(path = "/{order_id}")
+    public @ResponseBody Optional<Order> getOrder(@PathVariable String order_id) {
+        return orderRepository.findById(Integer.valueOf(order_id));
     }
 }
