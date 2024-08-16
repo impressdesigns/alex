0f95d633c06592af9f9bf3bcc35bde177044f5fe
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-08-16 12:07:37 -0500

    Revert "Sadness"
    This reverts commit 818b77beca706f1a7df44beae5af76c351423fe7.

--- src/main/java/com/impressdesigns/alex/orders/OrderController.java
+++ src/main/java/com/impressdesigns/alex/orders/OrderController.java
@@ -1,6 +1,5 @@
 package com.impressdesigns.alex.orders;
 
-import io.sentry.Sentry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,10 +24,6 @@ public class OrderController {
 
     @GetMapping(path = "/{order_id}")
     public @ResponseBody Optional<Order> getOrder(@PathVariable String order_id) {
-        final Optional<Order>[] result = new Optional[1];
-        Sentry.metrics().timing("load_order", () -> {
-            result[0] = orderRepository.findById(Integer.valueOf(order_id));
-        });
-        return result[0];
+        return orderRepository.findById(Integer.valueOf(order_id));
     }
 }
