818b77beca706f1a7df44beae5af76c351423fe7
Bradley Reynolds <bradley.reynolds@impressdesigns.com>
2024-08-16 09:32:07 -0500

    Sadness
    Signed-off-by: Bradley Reynolds <bradley.reynolds@impressdesigns.com>

--- src/main/java/com/impressdesigns/alex/orders/OrderController.java
+++ src/main/java/com/impressdesigns/alex/orders/OrderController.java
@@ -1,5 +1,6 @@
 package com.impressdesigns.alex.orders;
 
+import io.sentry.Sentry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,10 @@ public class OrderController {
 
     @GetMapping(path = "/{order_id}")
     public @ResponseBody Optional<Order> getOrder(@PathVariable String order_id) {
-        return orderRepository.findById(Integer.valueOf(order_id));
+        final Optional<Order>[] result = new Optional[1];
+        Sentry.metrics().timing("load_order", () -> {
+            result[0] = orderRepository.findById(Integer.valueOf(order_id));
+        });
+        return result[0];
     }
 }
